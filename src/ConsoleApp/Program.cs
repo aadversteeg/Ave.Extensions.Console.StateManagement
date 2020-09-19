c24546b044002eeb127697171a2f228f3ec997be
Aad Versteeg <aad.versteeg@transavia.com>
2020-09-19 16:43:33 +0200

    style: some comments

--- src/ConsoleApp/Program.cs
+++ src/ConsoleApp/Program.cs
@@ -7,20 +7,25 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            // determine root folder for saving session data:
+            // determine root folder for saving session data
             var path = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "SessionState");
             var sessionStorage = new FileSessionStorage(new SystemDirectory(), new SystemFile(), new BinarySessionStateSerializer(), path);
 
+
+            // create Session for generating correct session key
             var session = new Session();
 
+            // create state manager
             var stateManager = new StateManager("SampleApp", session, sessionStorage);
 
             if(args.Length == 1)
             {
+                // read a value from state
                 Console.WriteLine($"Value for {args[0]} : {stateManager.GetValue<string>( args[0], "")}");
             } 
             else if(args.Length == 2)
             {
+                // set a value to state
                 stateManager.SetValue(args[0], args[1]);
                 stateManager.Save();
                 Console.WriteLine($"Setting value for {args[0]} to {args[1]}");
