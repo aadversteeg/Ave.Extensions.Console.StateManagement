cfe60d15a1ea2f181bba52cc91dd22deae05af90
Aad Versteeg <aad.versteeg@transavia.com>
2020-09-26 20:43:06 +0200

    Added unit tests for StateScope

--- src/UnitTests.Extensions.Console.StateManagement/StateManagerTests.cs
+++ src/UnitTests.Extensions.Console.StateManagement/StateManagerTests.cs
@@ -8,79 +8,89 @@ namespace UnitTests.Extensions.Console.StateManagement
 {
     public class StateManagerTests
     {
-        [Fact(DisplayName = "SMN-001: GetValue should return default value if key not present.")]
-        public void SM001()
+        [Theory(DisplayName = "SMN-001: GetValue should return default value if key not present.")]
+        [InlineData(StateScope.Session)]
+        [InlineData(StateScope.User)]
+        public void SM001(StateScope scope)
         {
             // arrange
             var sessionManagerMock = new Mock<ISessionManager>();
-            sessionManagerMock.Setup(m => m.Load(StateScope.Session))
+            sessionManagerMock.Setup(m => m.Load(scope))
                 .Returns(new Dictionary<string, object>());
 
             var stateManager = new StateManager(sessionManagerMock.Object);
 
             // act
-            var value = stateManager.GetValue(StateScope.Session, "test", 42);
+            var value = stateManager.GetValue(scope, "test", 42);
 
             // assert
             value.Should().Be(42);
         }
 
-        [Fact(DisplayName = "SMN-002: Value set with SetValue should be returned with GetValue.")]
-        public void SM002()
+        [Theory(DisplayName = "SMN-002: Value set with SetValue should be returned with GetValue.")]
+        [InlineData(StateScope.Session)]
+        [InlineData(StateScope.User)]
+        public void SM002(StateScope scope)
         {
             // arrange
             var sessionManagerMock = new Mock<ISessionManager>();
-            sessionManagerMock.Setup(m => m.Load(StateScope.Session))
+            sessionManagerMock.Setup(m => m.Load(scope))
                 .Returns(new Dictionary<string, object>());
 
             var stateManager = new StateManager(sessionManagerMock.Object);
-            stateManager.SetValue(StateScope.Session, "test", 123);
+            stateManager.SetValue(scope, "test", 123);
 
             // act
-            var value = stateManager.GetValue(StateScope.Session, "test", 42);
+            var value = stateManager.GetValue(scope, "test", 42);
 
             // assert
             value.Should().Be(123);
         }
 
-        [Fact(DisplayName = "SMN-003: HasKeyFor should return false if no value set for key.")]
-        public void SM003()
+        [Theory(DisplayName = "SMN-003: HasKeyFor should return false if no value set for key.")]
+        [InlineData(StateScope.Session)]
+        [InlineData(StateScope.User)]
+        public void SM003(StateScope scope)
         {
             // arrange
             var sessionManagerMock = new Mock<ISessionManager>();
-            sessionManagerMock.Setup(m => m.Load(StateScope.Session))
+            sessionManagerMock.Setup(m => m.Load(scope))
                 .Returns(new Dictionary<string, object>());
 
             var stateManager = new StateManager(sessionManagerMock.Object);
 
             // act
-            var result = stateManager.HasValueFor(StateScope.Session, "test");
+            var result = stateManager.HasValueFor(scope, "test");
 
             // assert
             result.Should().BeFalse();
         }
 
-        [Fact(DisplayName = "SMN-004: HasKeyFor should return true if value set for key.")]
-        public void SM004()
+        [Theory(DisplayName = "SMN-004: HasKeyFor should return true if value set for key.")]
+        [InlineData(StateScope.Session)]
+        [InlineData(StateScope.User)]
+        public void SM004(StateScope scope)
         {
             // arrange
             var sessionManagerMock = new Mock<ISessionManager>();
-            sessionManagerMock.Setup(m => m.Load(StateScope.Session))
+            sessionManagerMock.Setup(m => m.Load(scope))
                 .Returns(new Dictionary<string, object>());
 
             var stateManager = new StateManager(sessionManagerMock.Object);
 
-            stateManager.SetValue(StateScope.Session, "test", 123);
+            stateManager.SetValue(scope, "test", 123);
 
             // act
-            var result = stateManager.HasValueFor(StateScope.Session, "test");
+            var result = stateManager.HasValueFor(scope, "test");
 
             // assert
             result.Should().BeTrue();
         }
 
-        [Fact(DisplayName = "SMN-005: StateManager should load state using provided session key.")]
-        public void SM005()
+        [Theory(DisplayName = "SMN-005: StateManager should load state.")]
+        [InlineData(StateScope.Session)]
+        [InlineData(StateScope.User)]
+        public void SM005(StateScope scope)
         {
             // arrange
             var storedSessionState = new Dictionary<string, object>()
@@ -90,7 +100,7 @@ namespace UnitTests.Extensions.Console.StateManagement
             };
 
             var sessionManagerMock = new Mock<ISessionManager>();
-            sessionManagerMock.Setup(m => m.Load(StateScope.Session))
+            sessionManagerMock.Setup(m => m.Load(scope))
                 .Returns(storedSessionState);
 
             // act
@@ -98,13 +108,15 @@ namespace UnitTests.Extensions.Console.StateManagement
 
 
             // assert
-            sessionManagerMock.Verify(m => m.Load(StateScope.Session), Times.Once);
-            stateManager.GetValue<string>(StateScope.Session, "one").Should().Be("value one");
-            stateManager.GetValue<int>(StateScope.Session, "two").Should().Be(2);
+            sessionManagerMock.Verify(m => m.Load(scope), Times.Once);
+            stateManager.GetValue<string>(scope, "one").Should().Be("value one");
+            stateManager.GetValue<int>(scope, "two").Should().Be(2);
         }
 
-        [Fact(DisplayName = "SMN-006: StateManager should save state using provided session key.")]
-        public void SM006()
+        [Theory(DisplayName = "SMN-006: StateManager should save state.")]
+        [InlineData(StateScope.Session)]
+        [InlineData(StateScope.User)]
+        public void SM006(StateScope scope)
         {
             // arrange
             var storedSessionState = new Dictionary<string, object>()
@@ -114,26 +126,84 @@ namespace UnitTests.Extensions.Console.StateManagement
             };
 
             var sessionManagerMock = new Mock<ISessionManager>();
-            sessionManagerMock.Setup(m => m.Load(StateScope.Session))
+            sessionManagerMock.Setup(m => m.Load(scope))
                 .Returns(storedSessionState);
 
              IDictionary<string, object> persistedState = null;
 
-            sessionManagerMock.Setup(m => m.Save(StateScope.Session, It.IsAny<IDictionary<string, object>>()))
+            sessionManagerMock.Setup(m => m.Save(scope, It.IsAny<IDictionary<string, object>>()))
                 .Callback<StateScope, IDictionary<string, object>>((scope, state) => persistedState = state);
 
             var stateManager = new StateManager(sessionManagerMock.Object);
 
             // act
-            stateManager.SetValue(StateScope.Session, "One", "Value One");
+            stateManager.SetValue(scope, "One", "Value One");
             stateManager.Save();
 
             // assert
-            sessionManagerMock.Verify(m => m.Save(StateScope.Session, It.IsAny<IDictionary<string, object>>()), Times.Once);
+            sessionManagerMock.Verify(m => m.Save(scope, It.IsAny<IDictionary<string, object>>()), Times.Once);
 
             persistedState.Should().NotBeNull();
             persistedState.Should().ContainKey("One");
             persistedState["One"].Should().Be("Value One");
         }
+
+
+        [Fact(DisplayName = "SMN-007: StateManager should keep state of different scope separate.")]
+        public void SM007()
+        {
+            // arrange
+            var storedSessionState = new Dictionary<string, object>()
+            {
+                { "one", "session" },
+                { "two", 2 }
+            };
+
+            var storedUserState = new Dictionary<string, object>()
+            {
+                { "one", "user" },
+                { "three", 3 }
+            };
+
+            var sessionManagerMock = new Mock<ISessionManager>();
+            sessionManagerMock.Setup(m => m.Load(StateScope.Session))
+                .Returns(storedSessionState);
+
+            sessionManagerMock.Setup(m => m.Load(StateScope.User))
+                .Returns(storedUserState);
+
+            IDictionary<string, object> persistedSessionState = null;
+            IDictionary<string, object> persistedUserState = null;
+
+            sessionManagerMock.Setup(m => m.Save(StateScope.Session, It.IsAny<IDictionary<string, object>>()))
+                .Callback<StateScope, IDictionary<string, object>>((scope, state) => persistedSessionState = state);
+
+            sessionManagerMock.Setup(m => m.Save(StateScope.User, It.IsAny<IDictionary<string, object>>()))
+                .Callback<StateScope, IDictionary<string, object>>((scope, state) => persistedUserState = state);
+
+            var stateManager = new StateManager(sessionManagerMock.Object);
+
+            // act
+            stateManager.SetValue(StateScope.Session, "ten", "SessionTen");
+            stateManager.SetValue(StateScope.User, "ten", "UserTen");
+            stateManager.Save();
+
+            // assert
+            sessionManagerMock.Verify(m => m.Save(StateScope.Session, It.IsAny<IDictionary<string, object>>()), Times.Once);
+            sessionManagerMock.Verify(m => m.Save(StateScope.User, It.IsAny<IDictionary<string, object>>()), Times.Once);
+
+            persistedSessionState.Should().NotBeNull();
+            persistedSessionState["one"].Should().Be("session");
+            persistedSessionState["two"].Should().Be(2);
+            persistedSessionState.ContainsKey("three").Should().BeFalse();
+            persistedSessionState["ten"].Should().Be("SessionTen");
+
+            persistedUserState.Should().NotBeNull();
+            persistedUserState["one"].Should().Be("user");
+            persistedUserState.ContainsKey("two").Should().BeFalse();
+            persistedUserState["three"].Should().Be(3);
+            persistedUserState["ten"].Should().Be("UserTen");
+        }
+
     }
 }
